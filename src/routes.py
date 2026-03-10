b3c5882beb7efed5f40ed6c9fa407a3e2f33df99
Lucy <lu.anzoateguy@gmail.com>
2026-03-10 15:57:36 -0300

    feat: implement password analysis endpoint

---
+++ src/routes.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, request, jsonify
+from .analyzer import password_analyzer  
+
+api = Blueprint('api', __name__)
+
+@api.route('/analyze', methods=['POST'])
+def analyze():
+    data = request.get_json()
+    password = data.get('password', '')
+
+    result = password_analyzer(password)
+
+    return jsonify(result)
