453e1e5669e52be80b4eae334ece461288115d3f
Lucy <lu.anzoateguy@gmail.com>
2026-03-12 14:16:32 -0300

    chore: adjust paths for Render deploy

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 from flask import Flask, send_from_directory
 from routes import api 
 
-app = Flask(__name__, static_folder='../public')
+app = Flask(__name__, static_folder='public')
 
 app.register_blueprint(api)
 
 @app.route('/')
 def index():
-    return send_from_directory('../public', 'index.html')
+    return send_from_directory('public', 'index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
